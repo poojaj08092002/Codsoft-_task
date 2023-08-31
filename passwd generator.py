e4a89352dafed8c2fd8bbbba342d937ed9a67d45
poojaj08092002 <142136667+poojaj08092002@users.noreply.github.com>
2023-09-01 00:42:33 +0530

    Update passwd generator.py

--- passwd generator.py
+++ passwd generator.py
@@ -8,36 +8,46 @@ while ch!="no":
     2.medium
     3.strong"""
     lowercase=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    digit=[0,1,2,3,4,5,6,7,8,9]
+    digit=["0","1","2","3","4","5","6","7","8","9"]
     uppercase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     special=['!','@','#','$','%','^','&','*','(',')'',','"','/','?','{','}','[',']','|','=','+','.','<','>']
     if complexity=="weak":
-        i = 0
-        s = ""
-        while i < length:
-            c = random.choice(lowercase) +  random.choice(uppercase)
-            s += str(c)
-            i += 1
-        print("The generated password is:", s, end="")
+            i = 0
+            s = ""
+            u=""
+            while i <= length:
+                c = random.choice(lowercase) +  random.choice(uppercase)
+                s += str(c)
+                i += 1
+                d = random.choice(s)
+                u += d
+            print("The generated password is:",u, end="")
+
+
     elif complexity=="medium":
-        i = 0
-        s = ""
-        while i < length:
-            c = random.choice(lowercase) +  random.choice(uppercase)
-            s += str(c)
-            i += 1
-        print("The generated password is:", s, end="")
+            i = 0
+            s = ""
+            u=""
+            while i < length:
+                c = random.choice(lowercase) + random.choice(uppercase) + random.choice(digit)
+                s += str(c)
+                i += 1
+                d=random.choice(s)
+                u+=d
+            print("The generated password is:", u, end="")
     else:
         i = 0
         s = ""
+        u=""
         while i < length:
-            c = random.choice(lowercase) +  random.choice(uppercase)
+            c = random.choice(lowercase) +  random.choice(uppercase) +random.choice(digit) + random.choice(special)
             s += str(c)
             i += 1
-        print("The generated password is:", s, end="")
+            d = random.choice(s)
+            u += d
+        print("The generated password is:", u, end="")
     print('\n')
     ch=input("do you want to regenerate the password again(yes/no):")
-
 
 
 
